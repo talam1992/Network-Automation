0d32cca3d7bec74657d115b080184ce847ff1017
Timothy Lam <timothylam92@hotmail.co.uk>
2019-09-24 22:14:31 +0100

    Network Automaton using GNS3

---
+++ python31.py
@@ -0,0 +1,19 @@
+import getpass
+import telnetlib
+
+HOST = "192.168.122.71"
+user = input("Enter your telnet username: ")
+password = getpass.getpass()
+
+tn = telnetlib.Telnet(HOST)
+
+tn.read_until(b"Username:")
+tn.write(user.encode('ascii') + b"\n")
+if password:
+        tn.read_until(b"Password: ")
+        tn.write(password.encode('ascii') + b"\n")
+
+tn.write(b"enable\n")
+tn.write(b"cisco\n")
+tn.write(b"conf t\n")
+tn.write(b"int loop 0\n")
