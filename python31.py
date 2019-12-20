2ab6c657e38b5d3fc629053a2935b52b7aa802ff
talam1992 <timothylam92@hotmail.co.uk>
2019-12-20 14:12:45 +0000

    updated python31 and python32

--- python31.py
+++ python31.py
@@ -14,6 +14,16 @@ if password:
         tn.write(password.encode('ascii') + b"\n")
 
 tn.write(b"enable\n")
-tn.write(b"cisco\n")
+tn.write(b"password\n")
 tn.write(b"conf t\n")
 tn.write(b"int loop 0\n")
+tn.write(b"ip address 1.1.1.1 255.255.255.255\n")
+tn.write(b"int loop 1\n")
+tn.write(b"ip address 2.2.2.2 255.255.255.255\n")
+tn.write(b"router ospf 1\n")
+tn.write(b"network 0.0.0.0 255.255.255.255 area 0\n")
+tn.write(b"end\n")
+tn.write(b"exit\n")
+
+print(tn.read_all().decode('ascii'))
+
--- python32.py
+++ python32.py
@@ -1,3 +1,4 @@
+
 import getpass
 import telnetlib
 
@@ -14,6 +15,17 @@ if password:
         tn.write(password.encode('ascii') + b"\n")
 
 tn.write(b"enable\n")
-tn.write(b"cisco\n")
+tn.write(b"password\n")
 tn.write(b"conf t\n")
 tn.write(b"vlan 2\n")
+tn.write(b"name Python_VLAN_2\n")
+tn.write(b"vlan 3\n")
+tn.write(b"name Python_VLAN_3\n")
+tn.write(b"vlan 4\n")
+tn.write(b"name Python_VLAN_4\n")
+tn.write(b"vlan 5\n")
+tn.write(b"name Python_VLAN_5\n")
+tn.write(b"end\n")
+tn.write(b"exit\n")
+
+print(tn.read_all().decode('ascii'))
